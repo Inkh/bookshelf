d967e9788f73177658e4827d10e4a086f935ed2e
Jeff <jhwe92@gmail.com>
2017-08-04 21:24:17 -0700

    temporarily fixed searchresult component author display and tweaked the layout

--- src/searchresult.js
+++ src/searchresult.js
@@ -23,28 +23,31 @@ class SearchResults extends Component {
       }
 
       return(
-        <div className="search-books">
-          <div className="search-books-bar">
-            <Link to='/' className='close-search'>Back</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                      NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                      You can find these search terms here:
-                      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <div className='app'>
+          <div className="search-books">
+            <div className="search-books-bar">
+              <Link to='/' className='close-search'>Back</Link>
+                <div className="search-books-input-wrapper">
+                  {/*
+                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                        You can find these search terms here:
+                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                      you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input
-                  value={this.state.query}
-                  onChange={(event) => this.updateQuery(event.target.value)}
-                  type="text"
-                  placeholder="Search by title or author"
-                  />
+                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                        you don't find a specific author or title. Every search is limited by search terms.
+                  */}
+                  <input
+                    value={this.state.query}
+                    onChange={(event) => this.updateQuery(event.target.value)}
+                    type="text"
+                    placeholder="Search by title or author"
+                    />
+                </div>
               </div>
             </div>
-            <div className='list-books-content'>
-              <div>
+            <div className='search-books-results'>
+              <div className='list-books-content'>
+                <div>
                   <div className='bookshelf-books'>
                       <ol className='books-grid'>
                         {this.props.results.map((book, index) => (
@@ -66,9 +69,7 @@ class SearchResults extends Component {
                                 <p>{book.title}</p>
                               </div>
                               <div className='book-authors'>
-                                {book.authors.map((author, index) => (
-                                    <p key={index}>{author}</p>
-                                ))}
+                                {book.authors}
                               </div>
                             </li>
                           </div>
@@ -77,8 +78,8 @@ class SearchResults extends Component {
                     </div>
                 </div>
               </div>
+            </div>
           </div>
-
       )
     }
 }
