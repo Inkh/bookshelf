7d6c5beba545486b60ec01940b4d4c529af4dc28
Jeff <jhwe92@gmail.com>
2017-08-04 23:44:29 -0700

    changed select tag so it corresponds with correct shelf

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -15,17 +15,17 @@ class BookShelf extends Component {
                         <div className='book' key={book.id}>
                           <li>
                             <div className='book-top'>
-                              {book.imageLinks && 
+                              {book.imageLinks &&
                                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                               }
 
                               <div className='book-shelf-changer'>
-                                  <select>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
+                                  <select value={book.shelf} onChange={() => (console.log('hi'))}>
+                                    <option value="none" disabled>Move to...</option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
                                   </select>
                               </div>
                             </div>
